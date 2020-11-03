3a61f40be644500afaf815476cbf9998c370e81c
Shivam Singh <sskscie@gmail.com>
2020-11-03 14:36:01 +0530

    binary

---
+++ binary.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int sarr(int n)
+{
+    int counter{0};
+    while(n)
+    {
+        n = n/2;
+        counter++;
+    }
+    return counter;
+}
+int main()
+{
+    int n , s;
+    cin >> n;
+    s = sarr(n);
+    int arr[s];
+    for(int i{0}; n != 0 ; i++)
+        {
+            arr[i] = n%2;
+            n /= 2;
+        }
+    for(int i{s-1};i >=0 ; i--)
+        {
+            cout << arr[i] << " " ;
+        }
+
+}
